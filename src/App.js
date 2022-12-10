57f37f798189a79432aeedb88e480c5f2d394bf8
jna0829 <shinjinah@naver.com>
2022-12-10 14:55:33 +0900

    #3. 엔터버튼 클릭 이벤트 추가

--- src/App.js
+++ src/App.js
@@ -1,40 +1,58 @@
-
+import React, {useState, useEffect} from 'react'; 
 import './App.css';
 import Todo from './components/Todo';
 import { List } from '@mui/material';
 import AddTodo from './components/AddTodo';
 
-//화면에 렌더링할 할일데이터
-const items = [
-  {
-      id: 1,
-      title: 'hello world1',
-      done: true
-  },
-  {
-      id: 2,
-      title: 'hello world2',
-      done: false
-  },
-  {
-      id: 3,
-      title: '동영상 강의 보기',
-      done: false
-  }
-];
-
-// const todoItems = items.map((item) => {
-//   return <Todo item={item} />
-// });
-
-const todoItems = items.map(item => <Todo key={item.id} item={item} />);
-
 const App = () => {
+
+  const [itemList, setItemList] = useState([
+    {
+        id: 1,
+        title: '점심메뉴 고르기',
+        done: true
+    },
+    {
+        id: 2,
+        title: '책 읽기',
+        done: false
+    }
+    ,
+    {
+        id: 3,
+        title: '동영상 강의 보기',
+        done: false
+    }
+  ]);
+
+
+  const todoItems = itemList.map(item => <Todo key={item.id} item={item} />);
+
+  // AddTodo에게 전달할 함수
+  // 할 일 추가 처리 함수
+  const add = (item) => {
+
+      item.id = itemList.length + 1;
+      item.done = false;
+
+      // console.log('add 호출됨!!');
+      // console.log(item);
+
+      setItemList(itemList => itemList.concat(item));
+  };
+
+
+  useEffect(() => {
+      // console.log('재 렌더링!!');
+      // console.log(itemList);
+  }, [itemList]);
+
+
   return (
     <div className="App">
-      <AddTodo />
+      <AddTodo add={add} />
       <List>
-        {todoItems}
+          {todoItems}
       </List>
     </div>
   );
--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,19 +1,39 @@
 import React, {useState} from 'react';
 import {TextField, Paper, Button, Grid} from "@mui/material";
 
-function AddTodo() {
+function AddTodo({ add }) {
 
     // 사용자의 입력을 저장할 객체
     const [item, setItem] = useState({ title : '' })
+
+    //입력창 값이 변할 때마다 item객체 안에 title값 자동 저장
+    const changeHendler = e => {
+        // console.log(e.target.value);
+        setItem({ title : e.target.value });
+    };
+
+    // 입력버튼 클릭 이벤트 핸들러
+    const addClickHandler = e => {
+        // console.log('버튼 클릭');
+        add(item); //item은 setItem의 title value를 넣어준 것
+    };
+
+    // 엔터버튼 클릭 이벤트 핸들러
+    const enterHendler = e => {
+        if (e.key === 'Enter') {
+            addClickHandler();
+        }
+    };
+
 
     return (
         <Paper style={{ margin: 16, padding: 16 }}>
             <Grid container>
                 <Grid xs={11} md={11} item style={{paddingRight: 16}}>
-                    <TextField placeholder="Add Todo Here" fullWidth />
+                    <TextField placeholder="Add Todo Here" fullWidth onChange={changeHendler} onKeyUp={enterHendler} value={item.title} />
                 </Grid>
                 <Grid xs={1} md={1} item>
-                    <Button fullWidth color="secondary" variant="outlined">
+                    <Button fullWidth color="secondary" variant="outlined" onClick={addClickHandler} >
                         +
                     </Button>
                 </Grid>
